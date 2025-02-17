156ea6c0f56e57b09e60371b98be4dc1d861b2c2
Frank-Lavoie <francis.lavoie79@outlook.com>
2025-02-17 10:04:25 -0500

    Test firebase fonctionnel.

--- app/src/main/java/com/example/recipio/MainActivity.kt
+++ app/src/main/java/com/example/recipio/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.recipio
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getString
 import com.example.recipio.ui.theme.RecipioTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +26,76 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipioTheme {
+
+                Log.i(getString(R.string.app_name), "Log test")
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        Greeting(
+                            name = getString(R.string.app_name),
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        Button(
+                            onClick = {seedDatabase()},
+
+                            ) {
+                            Text("Seed database")
+                        }
+                        Button(
+                            onClick = {logUsers()},
+
+                            ) {
+                            Text("Log users")
+                        }
+                    }
                 }
             }
         }
+    }
+
+    private fun addUser(){
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "first" to "Francis",
+            "last" to "Lavoie",
+            "born" to 1815
+        )
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(ContextCompat.getString(this@MainActivity.baseContext, R.string.app_name), "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(ContextCompat.getString(this@MainActivity.baseContext,R.string.app_name), "Error adding document", e)
+            }
+    }
+
+    private fun logUsers(){
+        val db = Firebase.firestore
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(getString(this@MainActivity.baseContext, R.string.app_name), "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(getString(this@MainActivity.baseContext, R.string.app_name), "Error getting documents.", exception)
+            }
+    }
+
+    private fun seedDatabase(){
+        val recipe : Recipe = Recipe("Recette 1", "Description de la recette 1")
+        val db = Firebase.firestore
+
+        db.collection("recipes")
+            .add(recipe)
+            .addOnSuccessListener { documentReference ->
+                Log.d(ContextCompat.getString(this@MainActivity.baseContext, R.string.app_name), "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(ContextCompat.getString(this@MainActivity.baseContext,R.string.app_name), "Error adding document", e)
+            }
     }
 }
 
---
+++ app/src/main/java/com/example/recipio/Recipe.kt
@@ -0,0 +1,3 @@
+package com.example.recipio
+
+data class Recipe(val name : String, val description : String)
