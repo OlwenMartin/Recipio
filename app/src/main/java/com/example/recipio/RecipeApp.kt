52980a6bc5cf0c828467fdda4bdc826361fd6a0c
Frank-Lavoie <francis.lavoie79@outlook.com>
2025-02-25 14:52:14 -0500

    Footer, header et search amélioré.

--- app/src/main/java/com/example/recipio/RecipeApp.kt
+++ app/src/main/java/com/example/recipio/RecipeApp.kt
@@ -1,9 +1,14 @@
 package com.example.recipio
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -11,7 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -21,8 +31,11 @@ import com.example.recipio.ui.HomeScreen
 import com.example.recipio.ui.RecipeViewModel
 import com.example.recipio.ui.SearchScreen
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.recipio.ui.RecipeScreen
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 enum class RecipeApp(@StringRes val title: Int){
     Start(title = R.string.app_name),
@@ -38,12 +51,28 @@ fun RecipeApp(
 ) {
     val uiState by viewModel.uiState.collectAsState()
     Scaffold(
+        topBar = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 30.dp)
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "Recipe Image",
+                    modifier = Modifier
+                        .size(180.dp, 60.dp)
+                        .clip(RoundedCornerShape(12.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        },
         bottomBar = {
-            BottomAppBar {
+            BottomAppBar(containerColor = Color(0xFF9bc268)) {
                 Button(
                     onClick = {navController.navigate(RecipeApp.Home.name)}
                 ) {
-                    Text(stringResource(R.string.home))
+                    Text(stringResource(R.string.home), fontSize = 20.sp)
                 }
                 Button(
                     onClick = {
@@ -51,12 +80,12 @@ fun RecipeApp(
                         navController.navigate(RecipeApp.Search.name)
                     }
                 ) {
-                    Text(stringResource(R.string.search))
+                    Text(stringResource(R.string.search), fontSize = 20.sp)
                 }
                 Button(
                     onClick = {navController.navigate(RecipeApp.Recipe.name)}
                 ) {
-                    Text(stringResource(R.string.recipe))
+                    Text(stringResource(R.string.recipe), fontSize = 20.sp)
                 }
             }
         }
